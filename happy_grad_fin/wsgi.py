b62926e1f86b30e6c6456a3e5806640d22e182d8
Theos-the-dawg <moteo13@outlook.com>
2024-10-01 22:12:47 +0200

    setings-file-and-wsgi-for-vercel

--- happy_grad_fin/wsgi.py
+++ happy_grad_fin/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'happy_grad_fin.settings')
 
 application = get_wsgi_application()
+
+app = application
