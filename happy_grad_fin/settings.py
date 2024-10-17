142589cd9abdc11acdd7e700192bd8472f2856c2
Theos-the-dawg <moteo13@outlook.com>
2024-10-17 19:51:19 +0200

    deploy-postgres-db-on-branch

--- happy_grad_fin/settings.py
+++ happy_grad_fin/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,12 +79,27 @@ WSGI_APPLICATION = 'happy_grad_fin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     # #primary db
+#     # 'default': dj_database_url.config(
+#     #     default='postgresql://postgres:MVDoJonBrvdxAqWKMjGmoUKVZJXUWeRA@postgres-h9tq.railway.internal:5432/railway'
+#     # ),
+
+#     'sqlite': {  # Secondary database (SQLite)
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3'}
+# }
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgresql://postgres:eDsYtailQTTjiwXrOXImGKdZwbuduLnC@autorack.proxy.rlwy.net:48571/railway')
 }
+#OG sqlite file for db
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
