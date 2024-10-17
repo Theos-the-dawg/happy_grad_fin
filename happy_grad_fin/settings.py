f627f6077c8d7485ffbbaf0a3f86670efbe7a8cd
Theos-the-dawg <moteo13@outlook.com>
2024-10-17 21:31:25 +0200

    chage-student-nav-link-pre-test-db

--- happy_grad_fin/settings.py
+++ happy_grad_fin/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-ya-h7^qu2*%7s7$r0r*v403c!&6t7ds^p0q)iawx6+uz1ip1!)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,13 +78,11 @@ WSGI_APPLICATION = 'happy_grad_fin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
-import dj_database_url
 DATABASES = {
     #DATABASE 1 MAIN DB FOR SERVICE
-    'default': dj_database_url.parse('postgresql://postgres:eDsYtailQTTjiwXrOXImGKdZwbuduLnC@autorack.proxy.rlwy.net:48571/railway'),
+    #'default': dj_database_url.parse('postgresql://postgres:eDsYtailQTTjiwXrOXImGKdZwbuduLnC@autorack.proxy.rlwy.net:48571/railway'),
     # Secondary database (SQLite)
-    'sqlite': {  
+    'default': {  
         'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3'}
 }
